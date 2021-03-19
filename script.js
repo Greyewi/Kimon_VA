7a353e1d38e2bc34003753a093110d47bd9e2356
greyewi1 <greyewi@gmail.com>
2021-03-19 22:26:32 +0500

    add edit

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ const checkedFields = new Set([])
 
 const validation = (inputs) => {
   const errors = {}
-  if(!inputs[0].value) {
+  if (!inputs[0].value) {
     errors.name = "name must not be empty"
   }
-  if(inputs[2].value.length < 3) {
+  if (inputs[2].value.length < 3) {
     errors.genre = "genre must have more than 3"
   }
   if (!inputs[3].value) {
@@ -20,12 +20,30 @@ const validation = (inputs) => {
   return Object.keys(errors).length && errors
 }
 
+//****************CHECKED*******************//
+
+const addHandlersToCheckboxes = () => {
+  const myCheckedInputs = document.querySelectorAll('input[type=checkbox]')
+  myCheckedInputs.forEach(inputEl => {
+    inputEl.addEventListener('click', () => {
+      const currentId = inputEl.parentElement.querySelector('td').innerHTML
+      if (checkedFields.has(currentId)) {
+        checkedFields.delete(currentId)
+      } else {
+        checkedFields.add(currentId)
+      }
+      console.log(checkedFields)
+      onRender(mockUp)
+    })
+  })
+}
+
 const addNewEntry = (dataArray, event) => {
-  event.preventDefault();
+  event && event.preventDefault()
   //create new object
   let newObject = {id: dataArray.length + 1}
   let formElements = document.getElementsByClassName('form-el')
-  if(validation(formElements)) {
+  if (validation(formElements)) {
     return alert(Object.values(validation(formElements)).join(', '))
   }
 
@@ -44,9 +62,9 @@ const sortTable = (element, tableData) => {
   const fieldName = element.innerHTML, direction = element.getAttribute('data-direction')
 
   onRender(tableData.sort((a, b) => {
-    if(a[fieldName] < b[fieldName]) {
+    if (a[fieldName] < b[fieldName]) {
       return 1 * direction
-    } else if(a[fieldName] > b[fieldName]) {
+    } else if (a[fieldName] > b[fieldName]) {
       return -1 * direction
     } else {
       return 0
@@ -61,26 +79,41 @@ const onRender = dataArray => {
 
   dataArray.map(line => {
     const tr = document.createElement('tr')
-    for(let key in line){
+
+    for (let key in line) {
       const td = document.createElement('td')
-      td.innerHTML = line[key]
+      if (checkedFields.has(String(line['id'])) && key !== 'id') {
+        const editInput = document.createElement('input')
+        editInput.setAttribute('type', 'text')
+        editInput.setAttribute('value', line[key])
+        editInput.setAttribute('name', key)
+        td.append(editInput)
+      } else if (key === 'id') {
+        tr.setAttribute('id', 'line_' + line['id'])
+        td.innerHTML = line[key]
+      } else {
+        td.innerHTML = line[key]
+      }
       tr.append(td)
     }
-
     const input = document.createElement('input')
     input.setAttribute('type', 'checkbox')
-    input.checked = checkedFields.has(line.id)
+    input.checked = checkedFields.has(String(line.id))
     tr.append(input)
 
     tableBody.append(tr)
   })
+  addHandlersToCheckboxes()
 }
 
 onRender(mockUp)
 
 //event listener for submit button - add.
-const form = document.getElementById("myForm");
-form.addEventListener('submit', event => addNewEntry(mockUp, event))
+const form = document.getElementById("myForm")
+form.addEventListener('submit', event => {
+  newLine =
+  addNewEntry(mockUp, event, newLine)
+})
 
 const columnHeadings = document.querySelectorAll('thead > tr > th')
 
@@ -99,39 +132,63 @@ myInputSearch.addEventListener('input', () => {
 
 function searchName(value, data) {
   return data.filter((item) => {
-    for(let key in item) {
-      if(item[key].toString(10).toLowerCase().includes(value.toLowerCase())) return true
+    for (let key in item) {
+      if (item[key].toString(10).toLowerCase().includes(value.toLowerCase())) return true
     }
     return false
   })
 }
 
-//****************CHECKED*******************//
-
-const myCheckedInputs = document.querySelectorAll('input[type=checkbox]')
-myCheckedInputs.forEach(inputEl => {
-  inputEl.addEventListener('click', () => {
-    const currentId = inputEl.parentElement.querySelector('td').innerHTML
-    if(checkedFields.has(currentId)){
-      checkedFields.delete(currentId)
-    } else {
-      checkedFields.add(currentId)
-    }
-    console.log(checkedFields)
-  })
-})
-
 //****************DELETE*******************//
+
+function removeEntry(data) {
+  const newData = data.filter((item) => {
+    if (!checkedFields.has(item['id'].toString())) return true
+  })
+  return newData
+}
+
 const deleteBtn = document.getElementById('delete')
 deleteBtn.addEventListener('click', () => {
   const data = removeEntry(mockUp)
   onRender(data)
 })
 
-function removeEntry(data) {
-  return data.filter((item) => {
-    if(!checkedFields.has(item['id'].toString())) return true 
+//****************EDIT*******************//
+
+function edit(data) {
+  const editInputsArray = []
+  checkedFields.forEach(number => {
+    const editLineInputs = document.querySelectorAll(`#line_${number} > td > input[type=text]`)
+    const lineObject = {}
+    editLineInputs.forEach((input) => {
+      lineObject[input.getAttribute('name')] = input.value
+    })
+    lineObject.id = Number(number)
+    editInputsArray.push(lineObject)
   })
+
+  checkedFields.clear()
+
+  onRender(data.map(line => {
+    editInputsArray.map(changedLine => {
+      if (line.id === changedLine.id) {
+        for (let key in line) {
+          line[key] = changedLine[key]
+        }
+      }
+    })
+    return line
+  }))
 }
 
-//why doesn't it work after one delete?
+//****************NEW EDIT*******************//
+
+function newEdit(data) {
+  // loop with onRender with push edits lines
+  // loop with removeEntry
+  // sortTable by id
+}
+
+const editBtn = document.querySelector('#edit')
+editBtn.addEventListener('click', () => edit(mockUp))
